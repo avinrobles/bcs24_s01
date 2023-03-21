3f9b9c8d35b8e4c09382c48bb08509c22106966e
Avin Robles <avinrobles@gmail.com>
2023-03-21 10:16:58 +0800

    Push Main Activity code

---
+++ indexActivity.js
@@ -0,0 +1,23 @@
+console.log('Hello, Welcome to De La Salle University');
+
+let firstName = "Avin";
+let lastName = "Robles";
+let age = 20;
+let hobbies = ["play games", "play instruments",
+                "read books", "workout four times a week",
+                "watch & learn from videos/online courses"];
+let homeAddress = {
+    houseNumber: "B8 L4",
+    street: "Fatima Road",
+    village: "N/a",
+    barangay: "San Simon",
+    city: "Dasmarinas",
+    province: "Cavite",
+    zipCode: "4114",
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
